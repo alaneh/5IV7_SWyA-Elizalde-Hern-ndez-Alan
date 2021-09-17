4a0c49e5dcd8993af9b95098bac531a0e38c7cad
alaneh <aelizaldeh1900@alumno.ipn.mx>
2021-09-17 12:59:44 -0500

    Cambiar el js de la clase por el solicitado de tarea

--- 01 Cesar/js/cesar.js
+++ 01 Cesar/js/cesar.js
@@ -1,17 +1,18 @@
-var cesar = cesar || (function(){
-    var proceso = function(txt, desp, action){
-        var replace = (function(){
+var cesar = cesar || (function() {
+    var proceso = function(txt, desp, action) {
+        var replace = (function() {
             //primero necesito tener la matriz del alfabeto
             //hay que recorrar que el cifrado lo hace caracter por caracter
             var abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-                        'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 
-                        'x', 'y', 'z'];
+                'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+                'x', 'y', 'z'
+            ];
             var l = abc.length;
 
             //necesitamos obtener la posicion que va  a venir por parte 
             //de la llave privada
 
-            return function(c){
+            return function(c) {
                 //vamos a saber la posicion
                 var i = abc.indexOf(c.toLowerCase());
                 //necesitamos saber es donde estamos adentro de la matriz
@@ -20,20 +21,20 @@ var cesar = cesar || (function(){
                 //alert(c);
                 //alert(i);
 
-                if(i != -1){
+                if (i != -1) {
                     //primero obtenemos la posicion para el desp
                     var pos = i;
                     //que voy a hacer cifrar o descifrar
-                    if(action){
+                    if (action) {
                         //cifrar para adelante
                         pos += desp;
                         //como se va a mover
-                        pos -= (pos >= l)?l:0;
-                    }else{
+                        pos -= (pos >= l) ? l : 0;
+                    } else {
                         //descifrar para atras
                         pos -= desp;
                         //movimiento
-                        pos += (pos < 0)?l:0;
+                        pos += (pos < 0) ? l : 0;
                     }
                     return abc[pos];
 
@@ -42,20 +43,20 @@ var cesar = cesar || (function(){
             };
         })();
         //tenemos que saber que el texto este acorde al abc
-        var re = (/([a-z])/ig);
+        var re = (/([a-zñ])/ig);
         //una funcion que se encargue del intercambio
-        return String(txt).replace(re, function(match){
+        return String(txt).replace(re, function(match) {
             return replace(match);
         });
-        
+
     };
 
-    return{
-        encode : function(txt, desp){
+    return {
+        encode: function(txt, desp) {
             return proceso(txt, desp, true);
         },
 
-        decode : function(txt, desp){
+        decode: function(txt, desp) {
             return proceso(txt, desp, false);
         }
     };
@@ -63,14 +64,26 @@ var cesar = cesar || (function(){
 
 //funcion de cifrado
 
-function cifrar(){
-    document.getElementById("resultado").innerHTML =
-    cesar.encode(document.getElementById("cadena").value, 3);
+function cifrar() {
+    var desplazamiento = document.getElementById("desplazamiento").value;
+    var numeros = (/([0-9]*)/);
+    alert(numeros.test(desplazamiento));
+    if (numeros.test(desplazamiento)) {
+        numero = parseInt(desplazamiento) % 27;
+        document.getElementById("resultado").innerHTML =
+            cesar.encode(document.getElementById("cadena").value, numero);
+    }
+
 }
 
 //funcion de descifrado
 
-function descifrar(){
-    document.getElementById("resultado").innerHTML =
-    cesar.decode(document.getElementById("cadena").value, 3);
+function descifrar() {
+    var desplazamiento = document.getElementById("desplazamiento").value;
+    var numeros = (/([0-9]*)/);
+    if (numeros.test(desplazamiento)) {
+        numero = parseInt(desplazamiento) % 26;
+        document.getElementById("resultado").innerHTML =
+            cesar.decode(document.getElementById("cadena").value, numero);
+    }
 }
